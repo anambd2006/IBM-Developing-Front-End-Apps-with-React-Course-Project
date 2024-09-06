da2310b800953b3b93098b2cf5af6554d9c6a6cc
anam <anambd2006@gmail.com>
2024-09-07 02:29:36 +0600

    e-plant Shopping Cart

---
+++ src/AboutUs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-heading">
+        Discover Nature’s Best for Your Health and Beauty
+      </h1>{" "}
+      *
+      {/*   <p className="about-us-description">Unlock Nature’s Secrets for Health and Beauty!</p> */}
+      <p className="about-us-content">
+        Our online store is your trusted source for high-quality herbal
+        products, tea blends, and natural beauty essentials designed to help you
+        achieve balance and well-being in your daily life.
+      </p>
+      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      <p className="about-us-content">
+        Our products come from reputable suppliers and undergo thorough quality
+        checks to ensure only the best for your health and well-being. We use
+        only natural ingredients to ensure an authentic and effective
+        experience.
+      </p>
+      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      <p className="about-us-content">
+        Explore our product range and start your journey to natural wellness
+        today!
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ src/CartItem.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeItem, updateQuantity } from "./CartSlice";
+import "./CartItem.css";
+
+const CartItem = ({ onContinueShopping }) => {
+  const cart = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+
+  // Calculate total amount for all products in the cart
+  const calculateTotalAmount = () => {
+    return cart
+      .reduce((total, item) => {
+        // Parse the cost to a float (assumes cost is a string like "$10.99")
+        const itemCost = parseFloat(item.cost.replace("$", ""));
+        // Multiply the cost by the quantity
+        return total + item.quantity * itemCost;
+      }, 0)
+      .toFixed(2); // Format to two decimal places
+  };
+
+  const handleContinueShopping = (e) => {
+    // Call the function passed from the parent component to navigate back to the plant listing page
+    if (onContinueShopping) {
+      onContinueShopping();
+    }
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference");
+  };
+
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({ name: item.name, quantity: item.quantity - 1 })
+      );
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+  };
+
+  // Calculate total cost based on quantity for an item
+  const calculateTotalCost = (item) => {
+    const itemCost = parseFloat(item.cost.replace("$", ""));
+    return (item.quantity * itemCost).toFixed(2);
+  };
+
+  return (
+    <div className="cart-container">
+      <h2 style={{ color: "black" }}>
+        Total Cart Amount: ${calculateTotalAmount()}
+      </h2>
+      <div>
+        {cart.map((item) => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-quantity">
+                <button
+                  className="cart-item-button cart-item-button-dec"
+                  onClick={() => handleDecrement(item)}
+                >
+                  -
+                </button>
+                <span className="cart-item-quantity-value">
+                  {item.quantity}
+                </span>
+                <button
+                  className="cart-item-button cart-item-button-inc"
+                  onClick={() => handleIncrement(item)}
+                >
+                  +
+                </button>
+              </div>
+              <div className="cart-item-total">
+                Total: ${calculateTotalCost(item)}
+              </div>
+              <button
+                className="cart-item-delete"
+                onClick={() => handleRemove(item)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{ marginTop: "20px", color: "black" }}
+        className="total_cart_amount"
+      ></div>
+      <div className="continue_shopping_btn">
+        <button
+          className="get-started-button"
+          onClick={(e) => handleContinueShopping(e)}
+        >
+          Continue Shopping
+        </button>
+        <br />
+        <button
+          className="get-started-button1"
+          onClick={() => alert("Coming Soon")}
+        >
+          Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
